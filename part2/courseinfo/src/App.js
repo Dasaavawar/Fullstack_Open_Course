142e56fd2853bee5a80de7a4f87a521d2ccc9a01
Dasaavawar <dsaavedra@unal.edu.co>
2023-03-13 11:55:42 -0500

    part2

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,4 +1,44 @@
-import Course from './components/Course'
+const Course = ({course}) => {
+  return (
+    <>
+    <Header name={course.name}/>
+    <Contents course={course}/>
+    <Total course={course}/>
+    </>
+  )
+}
+
+const Header = ({name}) => {
+  return (
+    <h3>{name}</h3>
+  )
+}
+
+
+const Part = ({part, exercises}) => {
+  return (
+    <>
+    <p>{part} {exercises}</p>
+    </>
+  )
+}
+
+const Contents = ({course}) => {
+  return (
+    <>
+      {course.parts.map(cou => <Part key={cou.id} part={cou.name} exercises={cou.exercises}/>)}
+    </>
+  )
+}
+
+const Total = ({course}) => {
+  const totalexercises = course.parts.reduce((t, {exercises}) => t + exercises, 0)
+  return(
+    <>
+    <b>total of {totalexercises} exercises</b>
+    </>
+  )
+}
 
 const Courses = ({courses}) => {
   return (
@@ -56,7 +96,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Web development curriculum</h1>
+      <h2>Web development curriculum</h2>
       <Courses courses={courses}/>
     </div>
   )
